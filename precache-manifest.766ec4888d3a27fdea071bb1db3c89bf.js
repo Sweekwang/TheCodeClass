7c8910ba2537c0df73e2c5ab1858477e354cdd7f
Swee Kwang Chua <sweekwangchua@SweeKwang.local>
2020-08-29 15:07:56 +0800

    Updates

---
+++ precache-manifest.766ec4888d3a27fdea071bb1db3c89bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa19c875912b8f07ee8b943d86bacca9",
+    "url": "/TheCodeClass/index.html"
+  },
+  {
+    "revision": "5e2c603259f9308d068c",
+    "url": "/TheCodeClass/static/css/main.3af7b6b8.chunk.css"
+  },
+  {
+    "revision": "af8d47b5e9c94d1322ae",
+    "url": "/TheCodeClass/static/js/2.469af9c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TheCodeClass/static/js/2.469af9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2c603259f9308d068c",
+    "url": "/TheCodeClass/static/js/main.60b000f4.chunk.js"
+  },
+  {
+    "revision": "a0dbbab4bd6023601168",
+    "url": "/TheCodeClass/static/js/runtime-main.3ec180de.js"
+  },
+  {
+    "revision": "c614785c7ad32523b3f302011fe7e515",
+    "url": "/TheCodeClass/static/media/back_arrow.c614785c.webp"
+  },
+  {
+    "revision": "ec8b66d35d8f0232e9371e463b383f87",
+    "url": "/TheCodeClass/static/media/exit.ec8b66d3.png"
+  }
+]);
