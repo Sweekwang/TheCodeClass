912900e1b10059cfa7a091e4294a577ecf89fda6
Swee Kwang Chua <sweekwangchua@SweeKwang.local>
2020-08-31 14:25:34 +0800

    Updates

---
+++ precache-manifest.1b5e0c3612b8b400695c66b237ec53c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e72711866899eb9f94a29b7748cb7f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "563b39d9a851b62020a1",
+    "url": "./static/css/main.b918b039.chunk.css"
+  },
+  {
+    "revision": "12fd9f88d50c8d2e160b",
+    "url": "./static/js/2.1e69ab3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1e69ab3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563b39d9a851b62020a1",
+    "url": "./static/js/main.1662cac8.chunk.js"
+  },
+  {
+    "revision": "a6a0ef2857da0a58deed",
+    "url": "./static/js/runtime-main.02c0f9d1.js"
+  },
+  {
+    "revision": "c614785c7ad32523b3f302011fe7e515",
+    "url": "./static/media/back_arrow.c614785c.webp"
+  },
+  {
+    "revision": "ec8b66d35d8f0232e9371e463b383f87",
+    "url": "./static/media/exit.ec8b66d3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bb90025ade8473e95ff80cb86646da1.js"
+  "./precache-manifest.1b5e0c3612b8b400695c66b237ec53c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
